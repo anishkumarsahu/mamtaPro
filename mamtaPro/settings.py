be2c6b1673f1d5e6e6e12b772eadb4fd7612336b
anishkumarsahu <anishkumarsahu03@gmail.com>
2024-08-03 20:34:07 +0530

    optimized sw to download reports

--- mamtaPro/settings.py
+++ mamtaPro/settings.py
@@ -76,29 +76,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mamtaPro.wsgi.application'
 
-#
+
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
-#
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mamta',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'password',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mamta',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'USER': 'root',
+#         'PASSWORD': 'password',
+#     }
+# }
 
 
 
